85aedde6258c8ca4791a592bf70ddcdd572cb514
Gustavo Silva <gustavo.silva@codigree.com>
2019-09-05 16:59:45 +0100

    Allows reading json file from local directory
        It should still follow a very strict structure to work.
        Regardless, we are getting the necessary information to push the
        necessary details to each room.
    
    Resolves #1

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
-	"github.com/PuloV/ics-golang"
+	"io/ioutil"
+	"os"
 )
 
+const FILE_PATH string = "./data/data.json"
+
 func main() {
-	fmt.Println("vim-go")
+	file, err := ioutil.ReadFile(FILE_PATH)
+	if err != nil {
+		fmt.Println("Error reading file. Does it exist?")
+		os.Exit(1)
+	}
+
+	var schedule ScheduleObj
+	err = json.Unmarshal(file, &schedule)
+	if err != nil {
+		fmt.Printf("Error parsing JSON file: \n %s", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(schedule)
+	// 	day := schedule.conf.days[i]
+	// 	fmt.Printf("Got room %d. Printing start and end dates. \n", i)
+	// 	fmt.Println(day.day_start)
+	// 	fmt.Println(day.day_end)
+
 }
